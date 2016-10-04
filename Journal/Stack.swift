a7136237ed63f54ea5e5250b93c676658c6ab852
Andrew Madsen <andrew@openreelsoftware.com>
2016-10-04 16:45:00 -0600

    Fix incorrect/mismatched model file and NSPersistentContainer names.

--- Journal/Stack.swift
+++ Journal/Stack.swift
@@ -10,7 +10,7 @@ enum CoreDataStack {
 	
 	static let container: NSPersistentContainer = {
 		
-		let container = NSPersistentContainer(name: "PlaylistCoreData")
+		let container = NSPersistentContainer(name: "Journal")
 		container.loadPersistentStores() { (storeDescription, error) in
 			if let error = error as NSError? {
 				fatalError("Unresolved error \(error), \(error.userInfo)")
