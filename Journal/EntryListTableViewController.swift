fb2bb05d153548203cd68f770739cfad4a6fcbaa
Andrew Madsen <andrew@openreelsoftware.com>
2016-10-13 07:34:23 -0600

    Made EntryListTableViewController use NSFetchedResultsController.

--- Journal/EntryListTableViewController.swift
+++ Journal/EntryListTableViewController.swift
@@ -7,59 +7,98 @@
 //
 
 import UIKit
+import CoreData
 
-class EntryListTableViewController: UITableViewController {
+class EntryListTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 	
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        tableView.reloadData()
-    }
-    
-    // MARK: UITableViewDataSource/Delegate
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return EntryController.sharedController.entries.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
-
-        let entry = EntryController.sharedController.entries[indexPath.row]
-        
-        cell.textLabel?.text = entry.title
-
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            
-            let entry = EntryController.sharedController.entries[indexPath.row]
-            
-			EntryController.sharedController.remove(entry: entry)
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		fetchedResultsController.delegate = self
+		do {
+			try fetchedResultsController.performFetch()
+		} catch {
+			NSLog("Error starting fetched results controller: \(error)")
+		}
+	}
+	
+	// MARK: UITableViewDataSource/Delegate
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return fetchedResultsController.fetchedObjects?.count ?? 0
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+		
+		guard let entry = fetchedResultsController.fetchedObjects?[indexPath.row] else { return cell }
+		
+		cell.textLabel?.text = entry.title
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
 			
-            // Delete the row from the table view
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
+			if let entry = fetchedResultsController.fetchedObjects?[indexPath.row] {
+				EntryController.sharedController.remove(entry: entry)
+			}
+		}
+	}
+	
+	// MARK: NSFetchedResultsControllerDelegate
+	
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+	                didChange anObject: Any,
+	                at indexPath: IndexPath?,
+	                for type: NSFetchedResultsChangeType,
+	                newIndexPath: IndexPath?) {
+		switch type {
+		case .delete:
+			guard let indexPath = indexPath else { return }
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		case .insert:
+			guard let newIndexPath = newIndexPath else { return }
+			tableView.insertRows(at: [newIndexPath], with: .automatic)
+		case .move:
+			guard let indexPath = indexPath,
+				let newIndexPath = newIndexPath else { return }
+			tableView.moveRow(at: indexPath, to: newIndexPath)
+		case .update:
+			guard let indexPath = indexPath else { return }
+			tableView.reloadRows(at: [indexPath], with: .automatic)
+		}
+	}
 	
 	// MARK: Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-        if segue.identifier == "toShowEntry" {
-            
-            if let detailViewController = segue.destination as? EntryDetailViewController {
-                
-                // Following line forces the view from Storyboard to load UI elements to make available for testing
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		
+		if segue.identifier == "toShowEntry" {
+			
+			if let detailViewController = segue.destination as? EntryDetailViewController {
+				
+				// Following line forces the view from Storyboard to load UI elements to make available for testing
 				detailViewController.loadViewIfNeeded()
 				
-                if let selectedRow = tableView.indexPathForSelectedRow?.row {
-                    let entry = EntryController.sharedController.entries[selectedRow]
+				if let selectedRow = tableView.indexPathForSelectedRow?.row,
+					let entry = fetchedResultsController.fetchedObjects?[selectedRow] {
 					detailViewController.entry = entry
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
+	
+	// MARK: Properties
+	
+	let fetchedResultsController: NSFetchedResultsController<Entry> = {
+		let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+		fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: true)]
+		
+		return NSFetchedResultsController(fetchRequest: fetchRequest,
+		                                  managedObjectContext: CoreDataStack.context,
+		                                  sectionNameKeyPath: nil,
+		                                  cacheName: nil)
+	}()
 }
